2e4553d5cc6e6f3733807cfbcd5f575929b18895
dhemeira <peterlakics2@gmail.com>
2026-08-03 19:22:51 +0200

    Setup new design system

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app.tsx';
-import '@fontsource-variable/inter/index.css';
+import '@fontsource-variable/plus-jakarta-sans/index.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Root element #root not found');
