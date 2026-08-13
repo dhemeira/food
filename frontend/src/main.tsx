a76740da99f4239818cb07e39d57d7b151cf4331
dhemeira <peterlakics2@gmail.com>
2026-08-13 15:51:11 +0200

    Fix offline mode error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app.tsx';
 import '@fontsource-variable/plus-jakarta-sans/index.css';
+import { registerServiceWorker } from '~/pwa/push';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Root element #root not found');
+
+await registerServiceWorker();
 
 createRoot(root).render(
   <StrictMode>
