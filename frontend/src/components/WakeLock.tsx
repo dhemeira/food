557e897183678ba350a516ae7df47946210f9260
dhemeira <peterlakics2@gmail.com>
2026-09-08 17:01:38 +0200

    Update WakeLock

--- frontend/src/components/WakeLock.tsx
+++ frontend/src/components/WakeLock.tsx
@@ -1,26 +1,32 @@
-import { useCallback } from 'react';
+import { useEffect } from 'react';
 import { useWakeLock } from '~/hooks/useWakeLock';
 
 function WakeLock() {
   const { isSupported, isActive, request, release } = useWakeLock();
 
-  const toggle = useCallback(() => {
-    if (isActive) {
-      void release();
-    } else {
-      void request();
+  useEffect(() => {
+    if (!isSupported) {
+      return;
     }
-  }, [isActive, request, release]);
 
-  if (!isSupported) {
-    return null;
-  }
+    void request();
+
+    return () => {
+      void release();
+    };
+  }, [isSupported, request, release]);
 
   return (
-    <label>
-      <input type="checkbox" checked={isActive} onChange={toggle} />
-      Képernyő ébren tartása receptnézés közben
-    </label>
+    <div className="bg-background fixed top-2 right-2 rounded-full">
+      <div className="bg-accent/20 flex gap-1 rounded-full p-1">
+        <div
+          className={'h-1 w-1 rounded-full ' + (isSupported ? 'bg-success' : 'bg-danger')}
+          title={isSupported ? 'WakeLock támogatott' : 'WakeLock nem támogatott'}></div>
+        <div
+          className={'h-1 w-1 rounded-full ' + (isActive ? 'bg-success' : 'bg-danger')}
+          title={isActive ? 'WakeLock aktív' : 'WakeLock inaktív'}></div>
+      </div>
+    </div>
   );
 }
 
